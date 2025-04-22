6cc4b756d91703f6eb54ee6fb8a3cc95f7ea2a00
Oleksandr Chekrenov <olekchekrenov@gmail.com>
2025-04-22 20:46:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const cols = document.querySelectorAll(".col");
 
+//Генерация случайного цвета
 function generateRadnomColor() {
   const hexCodes = "0123456789ABCDEF";
+  let color = "";
 
-  
-  for (let i = 0; i < 6; i++) {    
-    color += 
+  for (let i = 0; i < 6; i++) {
+    color += hexCodes[Math.floor(Math.random() * hexCodes.length)];
   }
+  return "#" + color;
 }
 
-
-// Проверяем колонки 
+// Проверяем колонки
 function setRandomColors() {
   cols.forEach((col) => {
-    console.log(col);
+    col.style.background = generateRadnomColor();
   });
 }
 
